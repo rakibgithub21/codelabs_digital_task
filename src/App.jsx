c0751cb073994350f312a09bfe4054018080965a
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-07-03 14:56:32 +0600

    add banner page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
+import Banner from './component/Banner'
 import Navbar from './component/Navbar'
 
 function App() {
 
   return (
     <div className='container mx-auto bg-[#FBFBFB]'>
-     <Navbar/>
+      <Navbar />
+      <Banner/>
     </div>
   )
 }
---
+++ src/component/Banner.jsx
@@ -0,0 +1,18 @@
+import bannerImage from '../assets/Media/Rectangle 5.png'
+
+const Banner = () => {
+    const bannerStyle = {
+        backgroundImage: `linear-gradient(179deg, rgba(0, 193, 157, 0.00) -25.42%, rgba(2, 0, 67, 0.47) 107.11%), url(${bannerImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        width: '100%',
+        height: '600px' // Adjust height as needed
+    };
+    return (
+        <div style={bannerStyle} className='rounded-[48px]'>
+            
+        </div>
+    );
+};
+
+export default Banner;
