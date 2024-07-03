0c1612a5e3f7f580e824aa296c9d8191165ac9a4
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-07-03 16:34:45 +0600

    workin on who we are section

--- src/component/WhoWeAre.jsx
+++ src/component/WhoWeAre.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+
 
 const WhoWeAre = () => {
     return (
-        <div>
-            
+        <div className="grid grid-cols-2 my-20">
+
+            <div className="col-span-1">
+                <button className="py-[10px] bg-[#FFFFF5] rounded-[25px] px-[24px] border border-[#343268]">Who We Are</button>
+                <h3>We Help To Get Soultions</h3>
+                <p>We are proud to be a trusted healthcare provider in our community, and we look forward to serving you and your family with excellence, integrity, and compassion. Your health is our priority, and we are here for you every step of the way. We believe in treating each patient with dignity, respect, and empathy, ensuring that they receive the attention and care they deserve.</p>
+            </div>
+            <div className="col-span-1">
+
+            </div>
+
         </div>
     );
 };
